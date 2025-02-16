700c9dae5f240277f13d1c5c1bae111743be1cac
Serg Nazarchuk <tesejz@gmail.com>
2025-02-17 00:34:48 +0200

    update forms with permisions

--- src/app/(rs)/customers/form/CustomerForm.tsx
+++ src/app/(rs)/customers/form/CustomerForm.tsx
@@ -8,6 +8,9 @@ import { Button } from "@/components/ui/button"
 import { InputWithLabel } from "@/components/inputs/InputWithLabel"
 import { SelectWithLabel } from "@/components/inputs/SelectWithLabel"
 import { TextAreaWithLabel } from "@/components/inputs/TextAreaWithLabel"
+import { CheckboxWithLabel } from "@/components/inputs/CheckboxWithLabel"
+
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
 
 import { StatesArray } from "@/constants/StatesArray"
 
@@ -18,7 +21,12 @@ type Props = {
     customer?: selectCustomerSchemaType,
 }
 
+
 export default function CustomerForm({ customer }: Props) {
+    const { getPermission, isLoading } = useKindeBrowserClient();
+    const isManager = !isLoading && getPermission('manager')?.isGranted;
+
+
     const defaultValues: insertCustomerSchemaType = {
         id: customer?.id ?? 0,
         firstName: customer?.firstName ?? '',
@@ -45,99 +53,106 @@ export default function CustomerForm({ customer }: Props) {
 
     return (
         <div className="flex flex-col gap-1 sm:px-8">
-            <div>
-                <h2 className="text-2xl font-bold">
-                    {customer?.id ? "Edit" : "New"} Customer Form
-                </h2>
-            </div>
-            <Form {...form}>
-                <form
-                    onSubmit={form.handleSubmit(submitForm)}
-                    className="flex flex-col md:flex-row gap-4 md:gap-8"
-                >
-                    <div className="flex flex-col gap-4 w-full max-w-xs">
-
-                        <InputWithLabel<insertCustomerSchemaType>
-                            fieldTitle="First Name"
-                            nameInSchema="firstName"
-                        />
-
-                        <InputWithLabel<insertCustomerSchemaType>
-                            fieldTitle="Last Name"
-                            nameInSchema="lastName"
-                        />
-
-                        <InputWithLabel<insertCustomerSchemaType>
-                            fieldTitle="Address 1"
-                            nameInSchema="address1"
-                        />
-
-                        <InputWithLabel<insertCustomerSchemaType>
-                            fieldTitle="Address 2"
-                            nameInSchema="address2"
-                        />
-
-                        <InputWithLabel<insertCustomerSchemaType>
-                            fieldTitle="City"
-                            nameInSchema="city"
-                        />
-
-                        <SelectWithLabel<insertCustomerSchemaType>
-                            fieldTitle="State"
-                            nameInSchema="state"
-                            data={StatesArray}
-                        />
-
-                    </div>
-
-                    <div className="flex flex-col gap-4 w-full max-w-xs">
-
-                        <InputWithLabel<insertCustomerSchemaType>
-                            fieldTitle="Zip Code"
-                            nameInSchema="zip"
-                        />
-
-                        <InputWithLabel<insertCustomerSchemaType>
-                            fieldTitle="Email"
-                            nameInSchema="email"
-                        />
-
-                        <InputWithLabel<insertCustomerSchemaType>
-                            fieldTitle="Phone"
-                            nameInSchema="phone"
-                        />
-
-                        <TextAreaWithLabel<insertCustomerSchemaType>
-                            fieldTitle="Notes"
-                            nameInSchema="notes"
-                            className="h-40"
-                        />
-
-                        <div className="flex gap-2">
-                            <Button
-                                type="submit"
-                                className="w-3/4"
-                                variant="default"
-                                title="Save"
-                            >
-                                Save
-                            </Button>
-
-                            <Button
-                                type="button"
-                                variant="destructive"
-                                title="Reset"
-                                onClick={() => form.reset(defaultValues)}
-                            >
-                                Reset
-                            </Button>
-                        </div>
-
-                    </div>
-
-                </form>
-            </Form>
-
+        <div>
+            <h2 className="text-2xl font-bold">
+                {customer?.id ? "Edit" : "New"} Customer {customer?.id ? `#${customer.id}` : "Form"}
+            </h2>
         </div>
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(submitForm)}
+                className="flex flex-col md:flex-row gap-4 md:gap-8"
+            >
+                <div className="flex flex-col gap-4 w-full max-w-xs">
+
+                    <InputWithLabel<insertCustomerSchemaType>
+                        fieldTitle="First Name"
+                        nameInSchema="firstName"
+                    />
+
+                    <InputWithLabel<insertCustomerSchemaType>
+                        fieldTitle="Last Name"
+                        nameInSchema="lastName"
+                    />
+
+                    <InputWithLabel<insertCustomerSchemaType>
+                        fieldTitle="Address 1"
+                        nameInSchema="address1"
+                    />
+
+                    <InputWithLabel<insertCustomerSchemaType>
+                        fieldTitle="Address 2"
+                        nameInSchema="address2"
+                    />
+
+                    <InputWithLabel<insertCustomerSchemaType>
+                        fieldTitle="City"
+                        nameInSchema="city"
+                    />
+
+                    <SelectWithLabel<insertCustomerSchemaType>
+                        fieldTitle="State"
+                        nameInSchema="state"
+                        data={StatesArray}
+                    />
+
+                </div>
+
+                <div className="flex flex-col gap-4 w-full max-w-xs">
+
+                    <InputWithLabel<insertCustomerSchemaType>
+                        fieldTitle="Zip Code"
+                        nameInSchema="zip"
+                    />
+
+                    <InputWithLabel<insertCustomerSchemaType>
+                        fieldTitle="Email"
+                        nameInSchema="email"
+                    />
+
+                    <InputWithLabel<insertCustomerSchemaType>
+                        fieldTitle="Phone"
+                        nameInSchema="phone"
+                    />
+
+                    <TextAreaWithLabel<insertCustomerSchemaType>
+                        fieldTitle="Notes"
+                        nameInSchema="notes"
+                        className="h-40"
+                    />
+
+                    {isLoading ? <p>Loading...</p> : isManager && customer?.id ? (
+                        <CheckboxWithLabel<insertCustomerSchemaType>
+                            fieldTitle="Active"
+                            nameInSchema="active"
+                            message="Yes"
+                        />) : null}
+
+                    <div className="flex gap-2">
+                        <Button
+                            type="submit"
+                            className="w-3/4"
+                            variant="default"
+                            title="Save"
+                        >
+                            Save
+                        </Button>
+
+                        <Button
+                            type="button"
+                            variant="destructive"
+                            title="Reset"
+                            onClick={() => form.reset(defaultValues)}
+                        >
+                            Reset
+                        </Button>
+                    </div>
+
+                </div>
+
+            </form>
+        </Form>
+
+    </div>
     )
 }
--- src/app/(rs)/tickets/form/TicketForm.tsx
+++ src/app/(rs)/tickets/form/TicketForm.tsx
@@ -6,21 +6,30 @@ import { Form } from "@/components/ui/form"
 import { Button } from "@/components/ui/button"
 
 import { InputWithLabel } from "@/components/inputs/InputWithLabel"
-// import { SelectWithLabel } from "@/components/inputs/SelectWithLabel"
+import { SelectWithLabel } from "@/components/inputs/SelectWithLabel"
 import { TextAreaWithLabel } from "@/components/inputs/TextAreaWithLabel"
 import { CheckboxWithLabel } from "@/components/inputs/CheckboxWithLabel"
 
 import { insertTicketSchema, type insertTicketSchemaType, type selectTicketSchemaType } from "@/zod-schemas/ticket"
 import { selectCustomerSchemaType } from "@/zod-schemas/customer"
 
+
 type Props = {
     customer: selectCustomerSchemaType,
     ticket?: selectTicketSchemaType,
+    techs?: {
+        id: string,
+        description: string,
+    }[],
+    isEditable?: boolean,
 }
 
 export default function TicketForm({
-    customer, ticket
+    customer, ticket, techs, isEditable = true
 }: Props) {
+
+    const isManager = Array.isArray(techs)
+
     const defaultValues: insertTicketSchemaType = {
         id: ticket?.id ?? "(New)",
         customerId: ticket?.customerId ?? customer.id,
@@ -42,61 +51,77 @@ export default function TicketForm({
 
     return (
         <div className="flex flex-col gap-1 sm:px-8">
-            <div>
-                <h2 className="text-2xl font-bold">
-                    {ticket?.id
-                        ? `Edit Ticket # ${ticket.id}`
+        <div>
+            <h2 className="text-2xl font-bold">
+                {ticket?.id && isEditable
+                    ? `Edit Ticket # ${ticket.id}`
+                    : ticket?.id
+                        ? `View Ticket # ${ticket.id}`
                         : "New Ticket Form"
-                    }
-                </h2>
-            </div>
-            <Form {...form}>
-                <form
-                    onSubmit={form.handleSubmit(submitForm)}
-                    className="flex flex-col md:flex-row gap-4 md:gap-8"
-                >
+                }
+            </h2>
+        </div>
+        <Form {...form}>
+            <form
+                onSubmit={form.handleSubmit(submitForm)}
+                className="flex flex-col md:flex-row gap-4 md:gap-8"
+            >
 
-                    <div className="flex flex-col gap-4 w-full max-w-xs">
+                <div className="flex flex-col gap-4 w-full max-w-xs">
 
-                        <InputWithLabel<insertTicketSchemaType>
-                            fieldTitle="Title"
-                            nameInSchema="title"
+                    <InputWithLabel<insertTicketSchemaType>
+                        fieldTitle="Title"
+                        nameInSchema="title"
+                        disabled={!isEditable}
+                    />
+
+                    {isManager ? (
+                        <SelectWithLabel<insertTicketSchemaType>
+                            fieldTitle="Tech ID"
+                            nameInSchema="tech"
+                            data={[{ id: 'new-ticket@example.com', description: 'new-ticket@example.com' }, ...techs]}
                         />
-
+                    ) : (
                         <InputWithLabel<insertTicketSchemaType>
                             fieldTitle="Tech"
                             nameInSchema="tech"
                             disabled={true}
                         />
+                    )}
 
+                    {ticket?.id ? (
                         <CheckboxWithLabel<insertTicketSchemaType>
                             fieldTitle="Completed"
                             nameInSchema="completed"
                             message="Yes"
+                            disabled={!isEditable}
                         />
+                    ) : null}
 
-                        <div className="mt-4 space-y-2">
-                            <h3 className="text-lg">Customer Info</h3>
-                            <hr className="w-4/5" />
-                            <p>{customer.firstName} {customer.lastName}</p>
-                            <p>{customer.address1}</p>
-                            {customer.address2 ? <p>{customer.address2}</p> : null}
-                            <p>{customer.city}, {customer.state} {customer.zip}</p>
-                            <hr className="w-4/5" />
-                            <p>{customer.email}</p>
-                            <p>Phone: {customer.phone}</p>
-                        </div>
-
+                    <div className="mt-4 space-y-2">
+                        <h3 className="text-lg">Customer Info</h3>
+                        <hr className="w-4/5" />
+                        <p>{customer.firstName} {customer.lastName}</p>
+                        <p>{customer.address1}</p>
+                        {customer.address2 ? <p>{customer.address2}</p> : null}
+                        <p>{customer.city}, {customer.state} {customer.zip}</p>
+                        <hr className="w-4/5" />
+                        <p>{customer.email}</p>
+                        <p>Phone: {customer.phone}</p>
                     </div>
 
-                    <div className="flex flex-col gap-4 w-full max-w-xs">
+                </div>
 
-                        <TextAreaWithLabel<insertTicketSchemaType>
-                            fieldTitle="Description"
-                            nameInSchema="description"
-                            className="h-96"
-                        />
+                <div className="flex flex-col gap-4 w-full max-w-xs">
 
+                    <TextAreaWithLabel<insertTicketSchemaType>
+                        fieldTitle="Description"
+                        nameInSchema="description"
+                        className="h-96"
+                        disabled={!isEditable}
+                    />
+
+                    {isEditable ? (
                         <div className="flex gap-2">
                             <Button
                                 type="submit"
@@ -116,12 +141,13 @@ export default function TicketForm({
                                 Reset
                             </Button>
                         </div>
+                    ) : null}
 
-                    </div>
+                </div>
 
-                </form>
-            </Form>
+            </form>
+        </Form>
 
-        </div>
+    </div>
     )
 }
